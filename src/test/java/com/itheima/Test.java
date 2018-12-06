9cce5a228346c176e81d3ccf3803a5ac1842bc6c
teng <1151106825@qq.com>
2018-12-06 22:30:54 +0800

    abc提交test

---
+++ src/test/java/com/itheima/Test.java
@@ -0,0 +1,14 @@
+package com.itheima;
+
+/**
+ * @author LT
+ * @version 1.0
+ * @description com.itheima
+ * @date 2018/9/17
+ */
+public class Test {
+    public void stard( String str){
+        str = str + "!";
+    }
+
+}
